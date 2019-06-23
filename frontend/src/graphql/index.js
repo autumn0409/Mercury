27e567c161ca4c48403ad95de876b19255674571
autumn0409 <b03502077@ntu.edu.tw>
2019-06-23 11:31:42 +0800

    move graphql folder to lib

--- frontend/src/graphql/index.js
+++
@@ -1,4 +0,0 @@
-export { USERS_QUERY } from './queries'
-export { POSTS_QUERY} from './queries'
-export { CREATE_POST_MUTATION, CREATE_LIKE_MUTATION } from './mutations'
-export { POSTS_SUBSCRIPTION, LIKES_SUBSCRIPTION } from './subscriptions'
