5c8727f2105df84bbc826ef22a6a0c6dace7cdfe
autumn0409 <b03502077@ntu.edu.tw>
2019-06-27 12:16:22 +0800

    Format codes in App.js

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
+import { Redirect, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Navbar from "../../containers/Navbar"
 import CreatePost from '../../components/create-post-page'
-import PostPage from '../../containers/PostPage'
 import FrontPage from "../../components/front-page"
 import Register from "../Register/Register"
 import Profile from '../../components/Profile'
 import Subs from '../../components/Subs'
 import SubPage from '../../containers/SubPage'
 import Logout from '../Logout/Logout'
-import { Redirect, BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Login from '../Login/Login'
 import PostContent from '../Post-Content'
 import EditPost from '../../components/EditPost'
@@ -25,11 +24,11 @@ class App extends Component {
     return (
       <Container>
         <Router>
-        <div style={{display:"flex"}}>
-            <Subs style={{background:"black",align:"left",float:"left"}}></Subs>
-             <Navbar></Navbar>
+          <div style={{ display: "flex" }}>
+            <Subs style={{ background: "black", align: "left", float: "left" }}></Subs>
+            <Navbar></Navbar>
           </div>
-         
+
           <Row>
             <Col>
               <Switch>
